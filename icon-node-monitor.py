df8a9fbd435c4842e8a1389cbe8671f8976f6ad2
Brian Li <brian@decrypto.net>
2019-09-24 16:37:57 -0400

    Update icon-node-monitor.py

--- icon-node-monitor.py
+++ icon-node-monitor.py
@@ -3,6 +3,7 @@
 #####################################
 
 import datetime
+import json
 import os
 import re
 import requests
@@ -46,37 +47,29 @@ time.sleep(2)
 #Make second response to API endpoint.
 response2 = requests.get(api_endpoint)
 
+#If node returns a 503 response, print message and exit.
+if response1.status_code == 503 or response2.status_code == 503:
+	print("Node is returning a 503 response.")
+	exit()
+
+#Encode response1 and response2 JSON, and extract block height.
+block_height1 = json.loads(response1.content.decode())['block_height']
+block_height2 = json.loads(response2.content.decode())['block_height']
+#print(block_height1)
+#print(block_height2)
+
 #Store node IP as a variable.
 node_ip_pattern = re.compile(r'\:\/\/(.*)\:9000')
 for node_ip_pattern_match in re.findall(node_ip_pattern, api_input):
 	pass
     #print(node_ip_pattern_match)
 
-#If node returns a 503 response, print message and exit.
-if response1.status_code == 503 or response2.status_code == 503:
-	print("Node is returning a 503 response.")
-	exit()
-
-#Decode data from response.
-data1 = response1.content.decode()
-data2 = response2.content.decode()
-
-#Define regex pattern to capture block height.
-block_height_pattern = re.compile(r'\"block_height\"\:([0-9]*)\,\"')
-
-#Extract block height number from data1 and data2.
-for block_height_pattern_match1 in re.findall(block_height_pattern, data1):
-	pass
-for block_height_pattern_match2 in re.findall(block_height_pattern, data2):
-	pass
-
 #If the block height of request 1 and 2 are equal, let the user know blocks aren't being produced.
-if block_height_pattern_match1 == block_height_pattern_match2:
+if block_height1 == block_height2:
 	#print("Uh oh. New blocks are not being produced.")
 	slack_alert_bad()
 #If the block height of request 1 is less than 2, let the user know blocks are being produced.
-elif block_height_pattern_match1 < block_height_pattern_match2:
-	pass
+elif block_height1 < block_height2:
 	#print("Yay! New blocks are being produced.")
 	slack_alert_good()
 #If any other condition is satisfied, something is very wrong.
