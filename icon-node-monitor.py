ca4f9bba142744f7d31abaa1e2c9a9f01506974a
Brian Li <brian@decrypto.net>
2019-09-24 18:35:22 -0400

    Change block height extraction method.

--- icon-node-monitor.py
+++ icon-node-monitor.py
@@ -19,7 +19,7 @@ def slack_alert_good():
 	client = slack.WebClient(token=slack_token)
 	client.chat_postMessage(
 	  channel="infrastructure-alerts",
-	  text="*ALERT: " + dt.strftime("%Y-%m-%d %H:%M:%S") + "*" "\n" + "RHIZOME node is operational."
+	  text="*ALERT: " + dt.strftime("%Y-%m-%d %H:%M:%S") + "*" "\n" + "Node " + node_ip + " is operational."
 	)
 
 def slack_alert_bad():
@@ -27,11 +27,12 @@ def slack_alert_bad():
 	client = slack.WebClient(token=slack_token)
 	client.chat_postMessage(
 	  channel="infrastructure-alerts",
-	  text="*ALERT: " + dt.strftime("%Y-%m-%d %H:%M:%S") + "*" "\n" + "RHIZOME node `" + node_ip_pattern_match + "` is stuck at block height " + block_height_pattern_match1 + "."
+	  text="*ALERT: " + dt.strftime("%Y-%m-%d %H:%M:%S") + "*" "\n" + "Node " + "`" + api_input + "`" " is stuck at block height " + str(block_height1) + "." + "\n" + "Please check the following server(s): " + node_ip + "."
 	)
 
 #Pass API endpoint from environment variable.
 api_input = os.environ["API_ENDPOINT"]
+
 #Add "http://" or "https://" if not specified in the environment variable.
 if "http://" in api_input or "https://" in api_input:
 	api_endpoint = api_input + "/api/v1/avail/peer"
@@ -41,16 +42,11 @@ else:
 #print(api_endpoint)
 
 #Make first response to API endpoint.
-response1 = requests.get(api_endpoint)
+response1 = requests.get(api_endpoint, timeout=30)
 #Wait 2 seconds.
 time.sleep(2)
 #Make second response to API endpoint.
-response2 = requests.get(api_endpoint)
-
-#If node returns a 503 response, print message and exit.
-if response1.status_code == 503 or response2.status_code == 503:
-	print("Node is returning a 503 response.")
-	exit()
+response2 = requests.get(api_endpoint, timeout=30)
 
 #Encode response1 and response2 JSON, and extract block height.
 block_height1 = json.loads(response1.content.decode())['block_height']
@@ -58,22 +54,22 @@ block_height2 = json.loads(response2.content.decode())['block_height']
 #print(block_height1)
 #print(block_height2)
 
-#Store node IP as a variable.
-node_ip_pattern = re.compile(r'\:\/\/(.*)\:9000')
-for node_ip_pattern_match in re.findall(node_ip_pattern, api_input):
-	pass
-    #print(node_ip_pattern_match)
+#Store node IP for response1 and response2 as variables.
+node_ip1 = json.loads(response1.content.decode())['peer_target'].rstrip(':7100')
+node_ip2 = json.loads(response2.content.decode())['peer_target'].rstrip(':7100')
+#Set value for node_ip depending on if the IPs for the two requests are the same.
+if node_ip1 == node_ip2:
+	node_ip = "`" + node_ip1 + "`"
+else:
+	node_ip = "`" + node_ip1 + "`"+ " and " + "`" + node_ip2 + "`"
 
 #If the block height of request 1 and 2 are equal, let the user know blocks aren't being produced.
 if block_height1 == block_height2:
 	#print("Uh oh. New blocks are not being produced.")
 	slack_alert_bad()
 #If the block height of request 1 is less than 2, let the user know blocks are being produced.
-elif block_height1 < block_height2:
-	#print("Yay! New blocks are being produced.")
-	slack_alert_good()
-#If any other condition is satisfied, something is very wrong.
 else:
-	print("REKT.")
+	#print("Yay! New blocks are being produced.")
+	slack_alert_bad()
 
 exit()
